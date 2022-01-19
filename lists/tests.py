2bbc9bac0c12367d6beb5d70df5ea859b3434554
heechankim <heechan.dev@gmail.com>
2022-01-20 01:53:19 +0900

    Test: Model for Items and Migrations

--- lists/tests.py
+++ lists/tests.py
@@ -3,9 +3,14 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+# 3rd-party
+from unittest import mock
+
 # application
 from lists.views import home_page
+from lists.models import Item
 
+@mock.patch('django.template.context_processors.get_token', mock.Mock(return_value='predicabletoken'))
 class HomaPageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
@@ -17,6 +22,7 @@ class HomaPageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -30,4 +36,23 @@ class HomaPageTest(TestCase):
             'home.html',
             {'new_item_text': '신규 작업 아이템'}
         )
-        self.assertEqual(response.content_decode(), expected_html)
+        self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
