5711be67d84dc804262acabfa7fcd5ef919f9093
heechankim <heechan.dev@gmail.com>
2022-01-19 20:52:00 +0900

    Test: check for row in list table, helper method

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # chan는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
@@ -57,13 +62,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn(
-            '2: 공작깃털을 이용해서 그물 만들기',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 찬은 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
         # 사이트는 그를 위한 특적 URL을 생성해준다.
