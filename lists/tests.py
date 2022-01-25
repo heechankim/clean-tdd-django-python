8723ad15e80becfbee97bf46e2a6606dc0f48e9f
heechankim <heechan.dev@gmail.com>
2022-01-25 21:20:49 +0900

    Test: Refactor all tests in lists, Add new url '/lists/new' so the view function added

--- lists/tests.py
+++ lists/tests.py
@@ -10,13 +10,9 @@ from unittest import mock
 from lists.views import home_page
 from lists.models import Item
 
+
 @mock.patch('django.template.context_processors.get_token', mock.Mock(return_value='predicabletoken'))
 class HomaPageTest(TestCase):
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -28,27 +24,26 @@ class HomaPageTest(TestCase):
         expected_html = render_to_string('home.html', request=request)
         self.assertEqual(response.content.decode(), expected_html)
 
+
+class NewListTest(TestCase):
+
     # DONE: 22.01.20 Refac: Test code is to long.
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-
-        response = home_page(request)
-
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, '신규 작업 아이템')
 
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,17 @@ from lists.models import Item
 # DONE: 22.01.10 : Feat: 모든 요청에 대한 비어 있는 요청은 저장하지 않는다.
 # DONE: 22.01.10 : Feat: 테이블에 아이템 여러개 표시하기
 # DONE: 22.01.10 : Feat: 하나 이상의 목록 지원하기
+
+
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
+
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
