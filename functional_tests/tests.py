7fc18cb345a75818722f2c48e0de7cdbf28b06a6
heechankim <heechan.dev@gmail.com>
2022-02-21 23:46:35 +0900

    Chore: url 어드민 주석처리

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 # selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -11,6 +12,23 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                print(arg)
+                cls.server_url = "http://" + arg.split('=')[1]
+                print(arg.split('='))
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         options = webdriver.ChromeOptions()
         self.browser = webdriver.Chrome(service=Service('./chromedriver'), options=options)
@@ -27,7 +45,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # chan는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
@@ -72,7 +90,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 프란시스가 홈페이지에 접속한다
         # 찬의 리스트는 보이지 않는다.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertNotIn('그물 만들기', page_text)
@@ -97,7 +115,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # 찬은 메인 페이지를 방문한다.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # 그는 입력 상자가 가운데 배치된 것을 본다.
