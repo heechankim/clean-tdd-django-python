4b551923854712882e79b1de0a72fb26b9846125
heechankim <heechan.dev@gmail.com>
2022-02-22 18:48:25 +0900

    Fix: 데이터베이스 및 스태틱 경로 수정

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../../database/db.sqlite3',
+        'NAME': BASE_DIR / '../database/db.sqlite3',
     }
 }
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('../../' + STATIC_URL)
+STATIC_ROOT = BASE_DIR.joinpath('../' + STATIC_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
